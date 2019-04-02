a9e3bbd1f2569159e3eaf2ab8f1eb7f84fc74068
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-02 12:06:24 +0200

    je recommence le projet en me basant sur les commits de Nico "https://github.com/dionation/wp_plugin"

--- wp-content/wordpress_dev_theme-creative_nis/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define ('INCLUDE_DIR', get_template_directory() . '/includes' );
-
-    require_once(INCLUDE_DIR . '/enqueue-script.php');
-    require_once(INCLUDE_DIR . '/menu.php');
-    require_once(INCLUDE_DIR . '/theme-setup.php');
-    require_once(INCLUDE_DIR . '/customizer.php');
-    require_once(INCLUDE_DIR . '/sidebars.php');
--- wp-content/wordpress_dev_theme-creative_nis/searchform.php
+++
@@ -1,5 +0,0 @@
-    <!-- search form -->
-    <form action="<?php echo get_site_url(); ?>">
-      <input type="text" name="s">
-      <button class="btn bg-white ml-3" type="submit" ><i class="fa fa-search"></i></button>
-    </form>
