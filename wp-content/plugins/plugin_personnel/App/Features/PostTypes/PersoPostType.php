cbba0f8c4866ab3ee1e870de4780794b63a8b7f7
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-03 10:28:40 +0200

    On remplace le slug qui était écrit en dur 'perso' par une variable. Cela facilite les modifications vs erreus.

--- wp-content/plugins/plugin_personnel/App/Features/PostTypes/PersoPostType.php
+++ wp-content/plugins/plugin_personnel/App/Features/PostTypes/PersoPostType.php
@@ -3,14 +3,35 @@
 namespace App\Features\PostTypes;
 
 class PersoPostType {
+    public static $slug = 'perso'; // on remplace le slug qui était écrit en dur 'recipe' par la variable, on fait cela car on va fair appel à notre function register_post_type par son indentifiant à plusieurs endroits, si jamais on décide de changer l'identifiant qui est 'recipe' par 'carotte' bah il faudra le changer a de nombreux endroit si on laisse cela en dur c'est pour cela qu'on à choisis de créer une variable,on change la valeur de la variable est tous les endroits ou son identifiant est utilisé sera mis à jours.
     public static function register() {
-        register_post_type('perso',
+        register_post_type( self::$slug,
         [
             'public' => true,
             'has_archive' => 'true',
             'labels' => [
                 'name' => 'Informations',
                 'singular_name' => 'Information',
+                'singular_name' => __('Information'),
+                'add_new' => __('Ajouter'),
+                'add_new_item' => __('Ajouter une information'),
+                'edit_item' => __('Modifier l\'information'),
+                'new_item' => __('Nouvelle information'),
+                'view_item' => __('Voir l\'information'),
+                'view_items' => __('Voir les informations'),
+                'search_items' => __('Rechercher des informations'),
+                'not_found' => __('Pas d\'informations trouvées.'),
+                'not_found_in_trash' =>('Pas d\'informations dans la corbeille.'),
+                'all_items' => __('Toutes les informations'),
+                'archives' => __('Informations archivées'),        
+                'filter_items_list' => __('Filtre d\'information'),
+                'items_list_navigation' => __('Navigation d\'information'),
+                'items_list' =>__('Liste d\'information'),
+                'item_published' => __('Information publiée.'),
+                'item_published_privately' =>__('Information publiée en privé.'),
+                'item_reverted_to_draft' =>__('L\'information est retournée au brouillon.'),
+                'item_scheduled' => __('Information planifiée.'),
+                'item_updated' =>__('Information mise à jours.'),
             ],
             'rewrite' => [
                 'slug' => 'info',
--- wp-content/plugins/plugin_personnel/plugin_personnel.php
+++ wp-content/plugins/plugin_personnel/plugin_personnel.php
@@ -8,7 +8,10 @@
  * Version: 0.1.0
  */
 
+
+// On use ici un namespace (celui qu'on créer ligne 2 dans autoload.php), l'avantage de cette écriture là c'est qu'on créer un 'ALIAS' avec la commande 'as' ce qui fait qu'a la ligne 24 on peut simplement écrire RecipePostType::class, pour l'exemple j'ai précisé que l'alias était RecipePostType mais a vrai dir c'est nécéssaire que si on modifie le nom de la class si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser si on écrivait :
 use App\Features\PostTypes\PersoPostType as PersoPostType;
 
-require_once(App\Features\PostTypes\PersoPostType.php);
+
+require_once('autoload.php'); // == require_once(App\Features\PostTypes\PersoPostType.php);
 add_action('init',[PersoPostType::class,'register']);
