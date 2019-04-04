2bb8c576c4a7203047832b9987ed844e584188a9
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-04 11:45:45 +0200

    Modification du contenu de la metabox . Un tableau avec != types de sources pour pouvoir trier les informations

--- wp-content/plugins/plugin_personnel/resources/views/metaboxes/perso-detail.html.php
+++ wp-content/plugins/plugin_personnel/resources/views/metaboxes/perso-detail.html.php
@@ -1,1 +1,13 @@
-<h3>Ma metabox est crée</h3>
+<table class="form-table">
+  <tr>
+    <th><?php _e("Source de l'information "); ?></th>
+    <td>
+      <select name="rat_time_information" id="rat_time_information">
+        <option value="">--</option>
+        <option value="livre">Livre</option>
+        <option value="journal">Journal</option>
+        <option value="reseau_sociaux">Réseau Sociaux</option>
+      </select>
+    </td>
+  </tr>
+</table>
