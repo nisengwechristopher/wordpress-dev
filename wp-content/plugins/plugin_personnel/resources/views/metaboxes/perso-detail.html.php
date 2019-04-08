1bbe780cd3045966d2cf9796d45171321901a19a
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-08 15:19:01 +0200

    Création d'une métabox from scratch avec un formulaire avec deux types d'inputs, j'ai vérifié que le ID du post pour les deux inputs est identique. Grace a la function the_ID(). Il me reste a sauvgarder les informations dans la base de donnée et les recupérer.

--- wp-content/plugins/plugin_personnel/resources/views/metaboxes/perso-detail.html.php
+++ wp-content/plugins/plugin_personnel/resources/views/metaboxes/perso-detail.html.php
@@ -4,7 +4,7 @@
     <td>
       <select name="perso_time_information" id="perso_time_information">
         <!-- On fait en sorte que l'option soit sélectionnée en fonction de la valeur récupérée dans la db. -->
-        <option value="">--</option>
+        <option value=""><?= $_POST["perso_time_information"] ?></option>
         <option <?= $time == 'livre' ? 'selected' : '' ?> value="livre">Livre</option>
         <option <?= $time == 'journal' ? 'selected' : '' ?> value="journal">Journal</option>
         <option <?= $time == 'reseau_sociaux' ? 'selected' : '' ?> value="reseau_sociaux">Réseau Sociaux</option>
@@ -13,10 +13,10 @@
   </tr>
 
   <tr>
-    <th><?php _e("Nombre de personne"); ?></th>
+    <th><?= _e("Nombre de personne"); ?></th>
     <td>
       <!-- Création du visuel en html ( un input ) dans le quel on met en valeur la variable qui à été envoyée par le compact dans RecipeDetailsMetabox.php ligne 37 -->
-      <input type="number" name="rat_nbr_personne" id="rat_nbr_personne" value="<?= $nbr_personne ?>" >
+      <input type="number" name="perso_nbr_personne" id="perso_nbr_personne" value="<?= $nbr_personne ?>" >
     </td>
   </tr>
   
